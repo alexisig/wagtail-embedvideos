e6ce68f9870da2f9f8740a20e11efa7fd37fa332
Alexis <alexis.at@l214.com>
2019-11-14 12:06:28 +0100

    f

--- wagtail_embed_videos/views/chooser.py
+++ wagtail_embed_videos/views/chooser.py
@@ -92,6 +92,6 @@ def embed_video_chosen(request, embed_video_id):
     embed_video = get_object_or_404(get_embed_video_model(), id=embed_video_id)
 
     return render_modal_workflow(
-        request, 'wagtail_embed_videos/chooser/embed_video_chosen.html', None,
-        {'embed_video_json': get_embed_video_json(embed_video)}
+        request, None, None,
+        None, json_data={'step': 'Video Chosen', 'result': get_embed_video_json(embed_video)}
     )
